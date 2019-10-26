c0655a860387ccd00b213dc18cc4a778f99d2316
carvillav <charles_alex20@hotmail.com>
2019-10-26 17:03:59 -0300

    migracion de algunas tablas

--- migracionCliente.sql
+++ migracionCliente.sql
@@ -9,20 +9,24 @@ select * from gd_esquema.Maestra
 insert into LOS_BORBOTONES.Role (Rol_Nombre)
 values  ('Administrativo'), ('Cliente'), ('Proveerdor')
 
-select * from LOS_BORBOTONES.Role
-
 /***Migracion tabla funcionalidad***/
 insert into LOS_BORBOTONES.Funcionalidad (Func_Nombre)
 values ('Login y seguridad'), ('ABM de Rol'), ('Registro de Usuario'), ('ABM de Cliente'), ('ABM de Proveedor'), 
 		('Cargar in'), ('Comprar Oferta'), ('Confeccion y publicacion de Ofertas'), ('Facturacion a Proveedor'), ('Listado Estadistico')
 
-select * from LOS_BORBOTONES.Funcionalidad
-
-/***Migracion tabla oferta***/--faltaria joinear con tabla proveedor para asociar la oferta con proveedor
-insert into LOS_BORBOTONES.Oferta (Oferta_Codigo, Oferta_Precio, Oferta_Precio_Ficticio, Oferta_Fecha, Oferta_Fecha_Venc, Oferta_Cantidad, Oferta_Descripcion)
-select distinct Oferta_Codigo ,Oferta_Precio, Oferta_Precio_Ficticio, Oferta_Fecha, Oferta_Fecha_Venc, Oferta_Cantidad, Oferta_Descripcion from gd_esquema.Maestra
+/***Migracion tabla oferta***/
+insert into LOS_BORBOTONES.Oferta (Oferta_Codigo, Oferta_Precio, Oferta_Precio_Ficticio, Oferta_Fecha, Oferta_Fecha_Venc, Oferta_Cantidad, Oferta_Descripcion, Provee_CUIT)
+select distinct Oferta_Codigo ,Oferta_Precio, Oferta_Precio_Ficticio, Oferta_Fecha, Oferta_Fecha_Venc, Oferta_Cantidad, Oferta_Descripcion, Provee_CUIT from gd_esquema.Maestra
 where Oferta_Codigo is not null 
 
-select * from LOS_BORBOTONES.Oferta
-select * from gd_esquema.Maestra
-where Oferta_Codigo is not null
+/***Migracion tipo de pago ***/
+insert into LOS_BORBOTONES.TipoDePago (Tipo_Pago_Desc)
+select distinct Tipo_Pago_Desc from gd_esquema.Maestra
+where Tipo_Pago_Desc is not null
+
+/***Migracion Carga***/
+
+insert into LOS_BORBOTONES.Carga (Cli_Dni, Carga_Credito, Carga_Fecha, Tipo_Pago_Id)
+select Cli_Dni, Carga_Credito, Carga_Fecha, Tipo_Pago_Id 
+from gd_esquema.Maestra g join LOS_BORBOTONES.TipoDePago t on (g.Tipo_Pago_Desc = t.Tipo_Pago_Desc)
+
