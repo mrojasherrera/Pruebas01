2ee347b077e93521eb9bb1ece76b260af2c4a885
mrojasherrera <mrojasherrera@gmail.com>
2019-11-02 11:17:30 -0300

    unificacion creacion-importacion de tablas

--- creacionTablas.sql
+++ creacionTablas.sql
@@ -186,3 +186,107 @@ Oferta_Cantidad numeric(18, 0),
 Oferta_Descripcion nvarchar(255),
 Provee_CUIT nvarchar(20)
 )
+
+
+/* *********** MIGRACIÓN DE DATOS ********* */
+
+/*** Migracion tabla cliente ***/
+
+insert into LOS_BORBOTONES.Cliente (Cli_Nombre, Cli_Apellido,Cli_Dni, Cli_Direccion, 
+			Cli_Telefono, Cli_Mail, Cli_Fecha_Nac, Cli_Ciudad)
+select distinct Cli_Nombre, Cli_Apellido, Cli_Dni, Cli_Direccion, Cli_Telefono, 
+			Cli_Mail, Cli_Fecha_Nac, Cli_Ciudad
+from gd_esquema.Maestra
+
+/*** Migracion tabla role ***/
+
+insert into LOS_BORBOTONES.Role (Rol_Nombre)
+	values  ('Administrativo'), ('Cliente'), ('Proveerdor')
+
+/*** Migracion tabla funcionalidad ***/
+
+insert into LOS_BORBOTONES.Funcionalidad (Func_Nombre)
+values ('Login y seguridad'), ('ABM de Rol'), ('Registro de Usuario'), ('ABM de Cliente'), ('ABM de Proveedor'), 
+		('Cargar in'), ('Comprar Oferta'), ('Confeccion y publicacion de Ofertas'), ('Facturacion a Proveedor'), ('Listado Estadistico')
+
+/*** Migracion tabla oferta ***/
+
+insert into LOS_BORBOTONES.Oferta (Oferta_Codigo, Oferta_Precio, Oferta_Precio_Ficticio, 
+			Oferta_Fecha, Oferta_Fecha_Venc, Oferta_Cantidad, Oferta_Descripcion, Provee_CUIT)
+select distinct Oferta_Codigo ,Oferta_Precio, Oferta_Precio_Ficticio, Oferta_Fecha, 
+			Oferta_Fecha_Venc, Oferta_Cantidad, Oferta_Descripcion, Provee_CUIT from gd_esquema.Maestra
+			where Oferta_Codigo is not null 
+
+/*** Migracion tipo de pago ***/
+
+insert into LOS_BORBOTONES.TipoDePago (Tipo_Pago_Desc)
+	select distinct Tipo_Pago_Desc from gd_esquema.Maestra
+	where Tipo_Pago_Desc is not null
+
+/*** Migracion Carga ***/
+
+insert into LOS_BORBOTONES.Carga (Cli_Dni, Carga_Credito, Carga_Fecha, Tipo_Pago_Id)
+	select Cli_Dni, Carga_Credito, Carga_Fecha, Tipo_Pago_Id 
+		from gd_esquema.Maestra g join LOS_BORBOTONES.TipoDePago t 
+		on (g.Tipo_Pago_Desc = t.Tipo_Pago_Desc)
+
+/*** Migracion factura ***/
+
+insert into LOS_BORBOTONES.Factura
+	(Factura_Nro, Factura_Fecha, Factura_Importe)
+	select Factura_Nro, Factura_Fecha, SUM(Oferta_Cantidad*Oferta_Precio) from gd_esquema.Maestra
+	where Oferta_Fecha_Compra is not null AND Factura_Nro is not null
+	group by Factura_Nro, Factura_Fecha
+	order by Factura_Nro
+
+
+/*** Migracion proveedores ***/
+
+insert into LOS_BORBOTONES.Proveedor
+	(Provee_RS, Provee_Dom, Provee_Ciudad, Provee_Telefono, Provee_CUIT,
+		Provee_Rubro)
+	select distinct Provee_RS, Provee_Dom, Provee_Ciudad, Provee_Telefono, Provee_CUIT,
+		Provee_Rubro
+		from gd_esquema.Maestra
+		where Provee_RS is not null
+
+/*** Migracion Cupones ***/
+
+insert into LOS_BORBOTONES.Cupon (
+	Oferta_Codigo, Cli_Dni, Cli_Dest_Nombre, Cli_Dest_Apellido, Cli_Dest_Dni,
+		Cli_Dest_Direccion, Cli_Dest_Telefono, Cli_Dest_Mail, Cli_Dest_Fecha_Nac,
+		Cli_Dest_Ciudad, Oferta_Fecha_Compra, Oferta_Entregado_Fecha, Oferta_Fecha_Venc,
+		Factura_Nro
+		
+	)
+	Select 
+		Oferta_Codigo, Cli_Dni, Cli_Dest_Nombre, Cli_Dest_Apellido, Cli_Dest_Dni,
+		Cli_Dest_Direccion, Cli_Dest_Telefono, Cli_Dest_Mail, Cli_Dest_Fecha_Nac,
+		Cli_Dest_Ciudad, Oferta_Fecha_Compra, Oferta_Entregado_Fecha, Oferta_Fecha_Venc, 
+		Factura_Nro
+		from gd_esquema.Maestra
+		where Oferta_Codigo is not null;
+
+
+/*** Creacion de Constrain ***/
+
+Cliente
+	Proveedor
+	Usuario
+	Rol_Usuario
+	Role
+	Func_Rol
+	Funcionalidad
+	Carga
+	TipoDePago
+	Cupon
+	Factura
+	Oferta
+
+ALTER TABLE LOS_BORBOTONES.
+ADD CONSTRAIN PK_
+PRIMARY KEY ();
+
+ALTER TABLE LOS_BORBOTONES.
+ADD CONSTRAINT FK_
+FOREIGN KEY (PersonID) REFERENCES Persons(PersonID);
